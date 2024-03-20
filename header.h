df92ef28957268fdc7487b3e8cb1d2ba65bc787f
Matei5 <147913718+Matei5@users.noreply.github.com>
2024-03-20 15:14:42 +0200

    commite

---
+++ header.h
@@ -0,0 +1,6 @@
+#ifndef PROIECT1_HEADER_H
+#define PROIECT1_HEADER_H
+
+
+
+#endif //PROIECT1_HEADER_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+git push -u origin main#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
